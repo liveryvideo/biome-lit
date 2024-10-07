33133b0e6f10e197c64fad25bdce9afcd05047ad
Peter Paul Elfferich <pp@exmachina.nl>
2024-10-07 21:16:32 +0200

    wip: add eslint-plugin-perfectionist

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import {configs as lit} from 'eslint-plugin-lit';
-import {configs as ts} from 'typescript-eslint';
+import perfectionist from 'eslint-plugin-perfectionist';
 import {configs as wc} from 'eslint-plugin-wc';
+import {configs as ts} from 'typescript-eslint';
 
-// Add eslint-plugin-*: perfectionist, unicorn, sonarjs, jsdoc, deprecation, vitest?
+// Add eslint-plugin-*: unicorn, sonarjs, jsdoc, deprecation, vitest?
 // TODO: Add eslint-plugin-tsdoc once that supports eslint v9 (or in Biome preferably)
 
 /**
@@ -11,52 +12,80 @@ import {configs as wc} from 'eslint-plugin-wc';
  * @type {import('eslint').Linter.Config}[]
  */
 export default [
+  // Directories to ignore
   {
     ignores: ['dist/', 'ext/', 'node_modules/'],
   },
+
+  // TypeScript support
   {
     files: ['**/*.ts'],
     ...ts.base
   },
-  lit['flat/all'],
+
+  // Web component support (https://www.webcomponents.org/)
   wc['flat/best-practice'],
+  {
+    rules: {
+      // Unnecessary when extending LitElement like we do
+      'wc/guard-super-call': 'off',
+    },
+  },
+
+  // Lit web component support (https://lit.dev/)
+  lit['flat/all'],
   {
     rules: {
       // We just use default lower case attribute names (e.g: not snake-case)
       'lit/attribute-names': 'off',
-      // Unnecessary when extending LitElement like we do
-      'wc/guard-super-call': 'off',
-    }  
-  }
-];
+    },
+  },
 
-// export const configs = [
-//   {
-//     files: ['**/*.{js,jsx,cjs,mjs,ts,tsx}'],
-//     rules: {
-//       // Require import file extensions instead of disabling them
-//       'import/extensions': ['error', 'always'],
-//       // Sort public members above protected instead of below private
-//       'perfectionist/sort-classes': [
-//         'error',
-//         {
-//           groups: [
-//             'index-signature',
-//             'static-property',
-//             'static-block',
-//             ['property', 'accessor-property'],
-//             ['protected-property', 'protected-accessor-property'],
-//             ['private-property', 'private-accessor-property'],
-//             'constructor',
-//             'static-method',
-//             'method',
-//             ['get-method', 'set-method'],
-//             'protected-method',
-//             'private-method',
-//             'unknown',
-//           ]
-//         }
-//       ]
-//     }
-//   },
-// ];
+  // Perfectionist formatting
+  {
+    // We can't override ignoreCase through settings when we use recommended-natural config so we'll do it this way
+    plugins: { perfectionist },
+    rules: {
+      ...Object.fromEntries(
+        Object.keys(perfectionist.rules).map(rule => [`perfectionist/${rule}`, ['error']]),
+      ),
+      // Sort public members above protected instead of below private
+      // and (public) property getter/setter methods along with plain properties
+      // and function-properties (e.g: bound methods) along with methods
+      'perfectionist/sort-classes': [
+        'error',
+        {
+          groups: [
+            'index-signature',
+            'static-property',
+            'protected-static-property',
+            'private-static-property',
+            'static-block',
+            ['property', 'accessor-property', 'get-method', 'set-method'],
+            ['protected-property', 'protected-accessor-property', 'protected-get-method', 'protected-set-method'],
+            ['private-property', 'private-accessor-property', 'private-get-method', 'private-set-method'],
+            'constructor',
+            ['static-method', 'static-function-property'],
+            ['protected-static-method', 'protected-static-function-property'],
+            ['private-static-method', 'private-static-function-property'],
+            ['method', 'function-property'],
+            ['protected-method', 'protected-function-property'],
+            ['private-method', 'private-function-property'],
+            'unknown',
+          ]
+        }
+      ],
+      // Leave import sorting up to Biome
+      'perfectionist/sort-imports': 'off',
+      'perfectionist/sort-named-imports': 'off',
+    },
+    settings: {
+      perfectionist: {
+        // Case has meaning, p.e: fooBar, fooBarCount, foobCount; you want foo to be grouped separately from foob
+        ignoreCase: false,
+        // Natural is preferable over plain alphabetic
+        type: 'natural',
+      }
+    }
+  },
+];
